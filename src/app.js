62176864139a0d9e0193daf950edbb788c7078cf
Szymon <sklemcz@gmail.com>
2021-08-01 22:04:25 +0200

    linking between pages

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter,  Route, Switch } from 'react-router-dom'
+import { BrowserRouter,  Route, Switch, Link, NavLink } from 'react-router-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
@@ -26,11 +26,22 @@ const HelpExpensePage = () => (
 )
 const NotFoundPage = () => (
     <div>
-        404 not found
+        404 not found - <Link to="/">Go home</Link>
     </div>
+)
+const Header = () => (
+    <header>
+        <h1>Expensify App</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Main page</NavLink>
+        <NavLink to="/create" activeClassName="is-active">create page</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">edit page</NavLink>
+        <NavLink to="/help" activeClassName="is-active">help page</NavLink>
+    </header>
 )
 const routes = (
     <BrowserRouter>
+    <div>
+        <Header/>
         <Switch>
         <Route path="/" component={ExpenseDashboarPage} exact={true}/>
         <Route path="/create" component={AddExpensePage} />
@@ -38,6 +49,7 @@ const routes = (
         <Route path="/help" component={HelpExpensePage} />
         <Route component={NotFoundPage} />
         </Switch>
+    </div>
     </BrowserRouter>
 )
 
